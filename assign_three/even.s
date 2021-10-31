95036bf0df919df7630133bbb4bfe7b4f4354eed
kskeshav <44917696+kskeshav@users.noreply.github.com>
2021-11-01 00:05:24 +0530

    Update even.s

--- assign_three/even.s
+++ assign_three/even.s
@@ -6,33 +6,28 @@
       
 __main  FUNCTION
 
-	  ; for byte addressable memory next successive location differ by 4
-	  
-	  MOV R0,#0x20000000
-	  MOV R8, #03
-	  MOV R9,#0x00000000	;stores the result.
-	  ADD R1, R0, #0x00000004 
-	  ADD R2, R1, #0x00000004
-	  ADD R3, R2, #0x00000004
-	  
-	  MOV R4, #012
-	  MOV R5, #015
-	  MOV R6, #017
-	  MOV R7, #022
+	; for byte addressable memory next successive location differ by 4
 
-	  STR R4, [R0]
-	  STR R5, [R1]
-	  STR R6, [R2]
-	  STR R7, [R3]
+	MOV R0,#0x20000000
+	MOV R8, #03
+	MOV R9,#0x00000000	;stores the result.
+	ADD R1, R0, #0x00000004 
+	ADD R2, R1, #0x00000004
+	ADD R3, R2, #0x00000004
 
-	  LDR R4, [R0]
-	  LDR R5, [R1]
-	  LDR R6, [R2]
-	  LDR R7, [R3]
+	MOV R4, #012
+	MOV R5, #015
+	MOV R6, #017
+	MOV R7, #022
+
+	STR R4, [R0]
+	STR R5, [R1]
+	STR R6, [R2]
+	STR R7, [R3]
 
 J1	LDR R4, [R0]
 	TST R4,#01      ; checking even number by taking and with 1.
-      BNE J2
+  	BNE J2
 	ADD R9,#01
 
 J2	ADD R0,R0,#0x4
